a96b8ee90e6f8dbd8087e54fffd50885aa27699d
yanfagc <hanzhdy@163.com>
2017-10-23 21:03:16 +0800

    修改用户角色回写的bug

--- upc-manager/src/main/java/org/hanzhdy/manager/form/mapper/FormDataMapperExt.java
+++ upc-manager/src/main/java/org/hanzhdy/manager/form/mapper/FormDataMapperExt.java
@@ -1,4 +1,16 @@
 package org.hanzhdy.manager.form.mapper;
 
+import org.apache.ibatis.annotations.Param;
+import org.hanzhdy.manager.form.vo.FormDataVo;
+
+import java.util.List;
+
 public interface FormDataMapperExt extends FormDataMapper {
+    /**
+     * 根据业务数据查询它的扩展属性（datatype与recordid）
+     * @param datatype
+     * @param recordid
+     * @return
+     */
+    List<FormDataVo> selectFormDataByRecord(@Param("datatype") String datatype, @Param("recordid") Long recordid);
 }
--- upc-manager/src/main/java/org/hanzhdy/manager/form/service/FormDataService.java
+++ upc-manager/src/main/java/org/hanzhdy/manager/form/service/FormDataService.java
@@ -1,13 +1,16 @@
 package org.hanzhdy.manager.form.service;
 
-import java.util.List;
-
 import org.hanzhdy.manager.form.mapper.FormDataMapperExt;
 import org.hanzhdy.manager.form.model.FormData;
 import org.hanzhdy.manager.form.model.FormDataExample;
+import org.hanzhdy.manager.form.vo.FormDataVo;
 import org.hanzhdy.manager.support.service.AbstractUpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description 表单值Service
@@ -19,11 +22,46 @@ public class FormDataService extends AbstractUpcService {
     @Autowired
     private FormDataMapperExt formDataMapperExt;
     
+    /**
+     * 根据业务数据查询它的扩展属性（单表查询，即查询结果不包括fieldcode）
+     * @param datatype
+     * @param recordid
+     * @return
+     */
     public List<FormData> queryFormDataAsList(String datatype, Long recordid) {
         FormDataExample example = new FormDataExample();
         FormDataExample.Criteria criteria = example.createCriteria();
         criteria.andDatatypeEqualTo(datatype);
         criteria.andRecordidEqualTo(recordid);
         return this.formDataMapperExt.selectByExample(example);
+    }
+    
+    /**
+     * 根据业务数据查询它的扩展属性（关联f_field_info表查询，查询结果中包含有fieldcode）
+     * @param datatype
+     * @param recordid
+     * @return
+     */
+    public List<FormDataVo> queryFormDataListByRecord(String datatype, Long recordid) {
+        return this.formDataMapperExt.selectFormDataByRecord(datatype, recordid);
+    }
+    
+    /**
+     * 根据业务数据查询它的扩展属性（关联f_field_info表查询，查询结果中包含有fieldcode）<br/>
+     * 返回的结果为Map数据
+     * @param datatype
+     * @param recordid
+     * @return
+     */
+    public Map<String, FormDataVo> queryFormDataMapByRecord(String datatype, Long recordid) {
+        List<FormDataVo> dataList = this.formDataMapperExt.selectFormDataByRecord(datatype, recordid);
+        Map<String, FormDataVo> resultMap = new HashMap<String, FormDataVo>();
+        if (dataList != null && !dataList.isEmpty()) {
+            for (FormDataVo data : dataList) {
+                resultMap.put(data.getFieldcode(), data);
+            }
+        }
+        
+        return resultMap;
     }
 }
